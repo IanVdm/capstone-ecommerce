3f749c5004ede55691c78e88685a8af3849dd4c8
IanVdm <ian@Ians-MacBook-Pro.local>
2023-02-21 12:25:04 +0100

    Added BrowserRouter and created the navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,31 @@
+// REACT
+import { Fragment } from "react";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+// ROUTES
+import { Outlet, Link } from "react-router-dom"
+
+// STYLES
+import './navigation.styles.scss';
+
+// NAVIGATION BAR (TOP LEVEL COMPONENT)
+const Navigation = () => {
+  
+    return (
+      <Fragment>
+        <div className="navigation">
+            <Link className="logo-container" to={'/'}>
+                <Logo className="logo"/>
+            </Link>
+            
+            <div className="nav-links-container">
+                <Link className="nav-link" to={'/shop'}>SHOP</Link> 
+            </div>
+        </div>
+
+        <Outlet />
+      </Fragment>
+    )
+}
+
+export default Navigation;
